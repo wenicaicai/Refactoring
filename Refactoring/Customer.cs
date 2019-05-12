4deb391c005cb8a3eb36730349c7822ad4a6096c
杨永财 <youngwenicai@outlook.com>
2019-05-12 12:07:56 +0800

    1.填充空类属性

--- Refactoring/Customer.cs
+++ Refactoring/Customer.cs
@@ -6,5 +6,23 @@ namespace Refactoring
 {
     class Customer
     {
+        private String _name;
+        private List<Rental> _rentals;
+
+        public Customer(String name,List<Rental> rentals)
+        {
+            _name = name;
+            _rentals = rentals;
+        }
+
+        public void addRental(Rental rental)
+        {
+            _rentals.Add(rental);
+        }
+
+        public String getName()
+        {
+            return _name;
+        }
     }
 }
--- Refactoring/Movie.cs
+++ Refactoring/Movie.cs
@@ -6,5 +6,32 @@ namespace Refactoring
 {
     class Movie
     {
+        private String _title;
+        private int _priceCode;
+
+        private static int CHILDRENS = 2;
+        private static int REGULAR = 0;
+        private static int NEW_RELEASE = 1;
+        public Movie(String title,int priceCode)
+        {
+            _title = title;
+            _priceCode = priceCode;
+        }
+
+        public int getPriceCode()
+        {
+            return _priceCode;
+        }
+
+        public void setPriceCode(int arg)
+        {
+            _priceCode = arg;
+        }
+
+        public String getTitle()
+        {
+            return _title;
+        }
+
     }
 }
