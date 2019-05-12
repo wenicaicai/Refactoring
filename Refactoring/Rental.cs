b2eee88cae036c3ea90d5951d3d5acaf374e0d49
杨永财 <youngwenicai@outlook.com>
2019-05-12 23:46:09 +0800

    1.初步完成第一个实例的重构 2.面对需求变更的代码变更，还需要进一步学习Martin  ...to be continue;

--- Refactoring/Rental.cs
+++ Refactoring/Rental.cs
@@ -24,5 +24,59 @@ namespace Refactoring
         {
             return _movie;
         }
+
+        /// <summary>
+        /// 计算金额
+        /// 
+        /// 把amountFor 转为 getCharge,修改参数名 and 去掉参数
+        /// 
+        /// tips：使用了MoveMethod
+        /// 
+        /// </summary>
+        /// <param name="aRental"></param>
+        /// <returns></returns>
+        public double getCharge()
+        {
+            double result = 0;
+            switch (getMovie().getPriceCode())
+            {
+                case 0:
+                    {
+                        result += 2;
+                        if (getDaysRented() > 2)
+                        {
+                            result += (getDaysRented() - 2) * 1.5;
+                        }
+                    }
+                    break;
+                case 1:
+                    {
+                        result += getDaysRented() * 3;
+                    }
+                    break;
+                case 2:
+                    {
+                        result += 1.5;
+                        if (getDaysRented() > 3)
+                        {
+                            result += (getDaysRented() - 3) * 1.5;
+                        }
+                    }
+                    break;
+            }
+            return result;
+        }
+
+        public int getFrequentRenterPoints()
+        {
+            if (getMovie().getPriceCode() == 1
+                && getDaysRented() > 1)
+            {
+                return 2;
+            }
+            return 1;
+        }
+
+
     }
 }
