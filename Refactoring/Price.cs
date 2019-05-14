8491a84375ccbb805cbed97291dc623bfaa9caa2
杨永财 <youngwenicai@outlook.com>
2019-05-14 08:19:34 +0800

    1.将Movie的价格的switch进行重构，将影片的价格独立出来 Martin say:"现在我们可以重新编译并测试，那些复杂的函数根本不知道外面的世界已经变了个样儿"

---
+++ Refactoring/Price.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Refactoring
+{
+    public abstract class Price
+    {
+        public abstract int getPriceCode();
+    }
+
+    public class ChildrensPrice : Price
+    {
+        public override int getPriceCode()
+        {
+            return 2;
+        }
+    }
+
+    public class NewReleasePrice : Price
+    {
+        public override int getPriceCode()
+        {
+            return 1;
+        }
+    }
+
+    public class RegularPrice : Price
+    {
+        public override int getPriceCode()
+        {
+            return 0;
+        }
+    }
+
+}
